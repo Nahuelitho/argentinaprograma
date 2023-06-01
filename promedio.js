495852a955e6042156045925f3bd56463e1bbe07
Nahuelitho <nahuel11905@gmail.com>
2023-06-01 18:14:04 -0300

    Practico0 subido por completo y finalizado, solo falta agregar a los ayudantes como personas a las cuales les contribuidores

---
+++ promedio.js
@@ -0,0 +1,7 @@
+function promedio(num1,num2,num3){
+    let promedio = (num1+num2+num3)/3;
+    return promedio;
+}
+console.log(promedio(5,7,9));
+console.log(promedio(-2,-3,-8));
+console.log(promedio(-1.2,5.3,-3.3));
